de154d726178544aab6fd887d902e3d6cd64524b
Sparsh Rai <cakelllkami@gmail.com>
2024-05-13 23:42:27 +0545

    added submit form function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 
 const App = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="container">
       <h1>Create Account</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="email"
           placeholder="Email"
@@ -17,11 +22,25 @@ const App = () => {
           required
         />
         <input
+          type="password"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          required
+        />
+        <input
           type="text"
           placeholder="Name"
           name="name"
           required
         />
+        <div>
+          <input
+            type="checkbox"
+            name="terms"
+            required
+          />
+          <label htmlFor="terms">I agree to the Terms and Conditions</label>
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
